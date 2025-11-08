a5db425612e6f4070669519fb14ac219a4dba906
hakunijun <trimini123456@gmail.com>
2025-11-08 11:02:55 +0700

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int tong(int a, int b){
+	return a+b;
+}
+
+int hieu(int a, int b){
+	return(a-b);
+}
+int tich(int a, int b){
+	return a*b;
+}
+
+float thuong(int a, int b);
+int main()
+{
+	int a, int b;
+	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
+	cout<<"Nhap a: ";
+	cin>>a;
+	cout<<"Nhap b: ";
+	cin>>b;
+	cout<<"Tong = "<<tong(a,b)<<endl;
+	cout<<"Hieu = "<<hieu(a,b)<<endl;
+	cout<<"Tich = " << tich(a, b) << endl;
+	cout<<endl;
+	cout<<"Tam2";
+	cout<<"Tam3";
+
+	system("pause");
+	return 0;
+}
